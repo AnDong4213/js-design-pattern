9bab895ae9bc7408ca0a747b0de6b226f1b20624
赵安东 <zhaoandong4213@sina.com>
2021-10-17 16:04:48 +0800

    单例模式

--- design-pattern-test/src/lib/4-pattern.js
+++ design-pattern-test/src/lib/4-pattern.js
@@ -2,7 +2,8 @@
 // jQuery-$('#div')
 class jQuery {
   constructor(selector) {
-    let dom = Array.from(document.querySelectorAll(selector));
+    // let dom = Array.from(document.querySelectorAll(selector));
+    let dom = Array.prototype.slice.call(document.querySelectorAll(selector));
     let len = dom ? dom.length : 0;
     for (let i = 0; i < len; i++) {
       this[i] = dom[i];
@@ -23,3 +24,67 @@ window.$ = function (selector) {
 };
 // console.log($("h1"));
 $("h1")[0].style.color = "#ccc";
+
+{
+  // 5-单例模式  jQuery中防止多次初始化
+  class SingleObject {
+    login() {
+      console.log("login...");
+    }
+  }
+  SingleObject.getInstance = (function () {
+    let instance;
+    return function () {
+      if (!instance) {
+        instance = new SingleObject();
+      }
+      return instance;
+    };
+  })();
+
+  let obj1 = SingleObject.getInstance();
+  obj1.login();
+  let obj2 = SingleObject.getInstance();
+  obj2.login();
+  console.log(obj1 === obj2); // true
+
+  class LoginForm {
+    constructor() {
+      this.state = "hide";
+    }
+
+    show() {
+      if (this.state === "show") {
+        console.log("已经显示");
+        return;
+      }
+      this.state = "show";
+      console.log("登录框显示成功");
+    }
+
+    hide() {
+      if (this.state === "hide") {
+        console.log("已经隐藏");
+        return;
+      }
+      this.state = "hide";
+      console.log("登录框隐藏成功");
+    }
+  }
+
+  LoginForm.getInstance = (function () {
+    let instance;
+    return function () {
+      if (!instance) {
+        instance = new LoginForm();
+      }
+      return instance;
+    };
+  })();
+
+  let login1 = LoginForm.getInstance();
+  login1.show();
+
+  let login2 = LoginForm.getInstance();
+  login2.show();
+}
